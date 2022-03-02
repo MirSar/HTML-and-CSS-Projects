953083c2249177af7c4b76725bbef77747df0761
Mirwais Sarwary <96671548+MirSar@users.noreply.github.com>
2022-03-02 14:22:52 -0800

    update basic_html_1 and basic_javascript_1 files, created basic_html_2

--- Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -1,1 +1,6 @@
 alert('Hello World! when ready push OK');
+function myFirstFunction() {
+  var String = 'kiss me because you know you LOVED IT!';
+  var result = String.fontcolor('green');
+  document.getElementById('lovedItMan').innerHTML = result;
+}
