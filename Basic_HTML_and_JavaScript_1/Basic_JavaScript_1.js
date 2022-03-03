f7470fd35c0f2f77705c985aa3b1ac9289daf767
Mirwais Sarwary <96671548+MirSar@users.noreply.github.com>
2022-03-03 13:23:50 -0800

    Added images to folder. created html-js-submission-1 file
    renamed file html-js-practice.html to jtml-js-submission-1.html.
    added required assignment tasks and a button feature

--- Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -1,6 +1,6 @@
-alert('Hello World! when ready push OK');
+alert('Hello World! ... when ready push OK');
 function myFirstFunction() {
-  var String = 'kiss me because you know you LOVED IT!';
+  var String = 'You made Grogu So HAPPY!';
   var result = String.fontcolor('green');
   document.getElementById('lovedItMan').innerHTML = result;
 }
